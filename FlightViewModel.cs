4b70c97eb3bcc55bfc7ab814bc93aeb25f785a2c
yarin-da <dado.yarin@gmail.com>
2021-03-29 22:58:38 +0300

    added a view model that uses our FlightModel

---
+++ FlightViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+
+namespace Adv_Prog_2
+{
+    internal class FlightViewModel : IViewModel
+    {
+        private IModel model;
+
+        public FlightViewModel()
+        {
+            INetClient netClient = new FlightNetClient();
+            ISimPlayer simPlayer = new FlightSimPlayer(netClient);
+            model = new FlightModel(netClient, simPlayer);
+            model.PropertyChanged +=
+                delegate(Object sender, PropertyChangedEventArgs e)
+                {
+                    NotifyPropertyChanged("VM_" + e.PropertyName);
+                };
+        }
+        public int VM_Frame {
+            get { return model.Frame; }
+            set { model.Frame = value; }
+        }
+        public int VM_FrameCount { 
+            get { return model.FrameCount; }
+            set { model.FrameCount = value; }
+        }
+        public string VM_TimerString {
+            get { return model.TimerString; }
+            set { model.TimerString = value; }
+        }
+        public string VM_SpeedString
+        {
+            get { return model.SpeedString; }
+            set { model.SpeedString = value; }
+        }
+        public float VM_Speed { 
+            get { return model.Speed; }
+            set { model.Speed = value; }
+        }
+        public bool VM_Connected { 
+            get { return model.IsConnected; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void Start()
+        {
+            model.Start();
+        }
+
+        public void Connect(int port, string server)
+        {
+            model.Connect(port, server);
+        }
+
+        public void Disconnect()
+        {
+            model.Disconnect();
+        }
+
+        public void Pause()
+        {
+            model.Pause();
+        }
+
+        public void Stop()
+        {
+            model.Stop();
+        }
+
+        public void SetFlightData(string filePath)
+        {
+            model.SetFlightDataFile(filePath);
+        }
+    }
+}
---
+++ IViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Adv_Prog_2
+{
+    interface IViewModel : INotifyPropertyChanged
+    {
+        public int VM_FrameCount { get; set; }
+        public int VM_Frame { get; set; }
+        public string VM_TimerString { get; set; }
+        public string VM_SpeedString { get; set; }
+        public float VM_Speed { get; set; }
+        public bool VM_Connected { get; }
+
+        public void Start();
+
+        public void Connect(int port, string server);
+
+        public void Disconnect();
+
+        public void Pause();
+
+        public void Stop();
+
+        public void SetFlightData(string filePath);
+    }
+}
