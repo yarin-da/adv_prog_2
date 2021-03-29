8ad3bc3add0b95b5d324d2f97fee252c2f4fde27
yarin-da <dado.yarin@gmail.com>
2021-03-29 22:57:44 +0300

    added a model that uses a netclient and a simplayer

---
+++ FlightModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Adv_Prog_2
+{
+    class FlightModel : IModel
+    {
+        private INetClient netClient;
+        private ISimPlayer simPlayer;
+
+        public FlightModel(INetClient netClient, ISimPlayer simPlayer)
+        {
+            this.netClient = netClient;
+            this.simPlayer = simPlayer;
+            simPlayer.PropertyChanged +=
+                delegate (Object sender, PropertyChangedEventArgs e)
+                {
+                    NotifyPropertyChanged(e.PropertyName);
+                };
+        }
+
+        public void Start()
+        {
+            simPlayer.Play();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public bool IsConnected { get { return netClient.IsConnected; } }
+        public void Connect(int port, string server) { netClient.Connect(port, server); }
+        public void Disconnect() { netClient.Disconnect(); }
+        public void Send(string data) { netClient.Send(data); }
+        public int FrameCount { 
+            get { return simPlayer.FrameCount; } 
+            set { simPlayer.FrameCount = value; }
+        }
+        public string TimerString {
+            get { return simPlayer.TimerString; }
+            set { simPlayer.TimerString = value; }
+        }
+        public string SpeedString {
+            get { return simPlayer.SpeedString; }
+            set { simPlayer.SpeedString = value; }
+        }
+        public float Speed {
+            get { return simPlayer.Speed; }
+            set { simPlayer.Speed = value; }
+        }
+        public int Frame {
+            get { return simPlayer.Frame; }
+            set { simPlayer.Frame = value; }
+        }
+        public void Play() { simPlayer.Play(); }
+        public void Pause() { simPlayer.Pause(); }
+        public void Stop() { simPlayer.Stop(); }
+        public void SetFlightDataFile(string filePath) { simPlayer.SetFlightDataFile(filePath); }
+    }
+}
---
+++ IModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Adv_Prog_2
+{
+    interface IModel : INotifyPropertyChanged
+    {
+        public void Start();
+        public bool IsConnected { get; }
+        public void Connect(int port, string server);
+        public void Disconnect();
+        public void Send(string data);
+        public int FrameCount { get; set; }
+        public string TimerString { get; set; }
+        public string SpeedString { get; set; }
+        public float Speed { get; set; }
+        public int Frame { get; set; }
+        public void Play();
+        public void Pause();
+        public void Stop();
+        public void SetFlightDataFile(string filePath);
+    }
+}
